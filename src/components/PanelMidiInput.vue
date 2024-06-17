779b9f9637ab9fe0afe46ade1a7ebf5b7705d9da
Gorkem Tosun <gorkemtosun@gmail.com>
2024-06-17 22:01:44 +0200

    Add midi input

---
+++ src/components/PanelMidiInput.vue
@@ -0,0 +1,54 @@
+<template lang='pug'>
+.panel
+  h4 Midi Input
+  select#midi-inputs(v-model='selectedInput' name='songs')
+    option(:key='input' v-html='input' :value='input' v-for='input in availableInputs')
+</template>
+
+<script>
+import WebMidi from 'webmidi';
+import { ref } from 'vue';
+import PanelTemplate from './PanelTemplate.vue';
+
+export default {
+  name: 'PanelMidiInput',
+  components: {
+    PanelTemplate,
+  },
+  setup() {
+    const availableInputs = ref(null)
+    const selectedInput = ref(null)
+    return {
+      availableInputs, selectedInput,
+    };
+  },
+  mounted() {
+    WebMidi.enable(() => {
+      console.log(WebMidi.inputs);
+      console.log(WebMidi.outputs);
+      this.availableInputs = WebMidi.inputs.map((input) => input.name);
+      this.selectedInput = this.availableInputs[0]; //eslint-disable-line
+    });
+  },
+};
+</script>
+
+<style>
+#left-hand-icon {
+  transform: scale(-1, 1);
+}
+.panel {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+.column {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+.input {
+  margin: 5px;
+}
+</style>
