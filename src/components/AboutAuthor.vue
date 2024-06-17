bc726fb406dfaba15e45097413409e42ae320c7d
Gorkem Tosun <gorkemtosun@gmail.com>
2024-06-17 15:41:57 +0200

    Add buy me coffee

--- src/components/AboutAuthor.vue
+++ src/components/AboutAuthor.vue
@@ -2,6 +2,8 @@
 #about
   label About The Author
   #icon-container
+    a(target='_blank' href='https://www.buymeacoffee.com/tosungo' id='buy-me-a-coffee')
+      img(src='https://cdn.buymeacoffee.com/buttons/v2/default-orange.png' alt='Buy Me A Coffee' style="height: 40px")
     a(target='_blank' href='https://github.com/Grkmus')
       font-awesome-icon(:icon="['fab', 'github']")
     a(target='_blank' href='https://twitter.com/tosungo')
@@ -21,5 +23,7 @@ export default {
 <style>
 #icon-container {
   display: flex;
+  align-items: center;
+  
 }
 </style>
