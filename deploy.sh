9044c09f76b7b42efa680912e6294a4f95c3ba99
Gorkem Tosun <gorkemtosun@gmail.com>
2024-06-17 22:45:18 +0200

    Correct the connection

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:grkmus/midi-vue.git master:production
+git push -f git@github.com:grkmus/web-piano.git main:production
 
 cd -
