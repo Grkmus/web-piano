f1051a27f9c18805f30535fca22162bce63f3503
Gorkem Tosun <gorkemtosun@gmail.com>
2024-06-21 14:30:52 +0200

    deploy

--- assets/batchSamplersUniformGroup.836f3365.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index.1af54b2a.js";import{U as o}from"./colorToUniform.d190ff8f.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
---
+++ assets/batchSamplersUniformGroup.fbea1051.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index.23139a8f.js";import{U as o}from"./colorToUniform.b3130495.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
